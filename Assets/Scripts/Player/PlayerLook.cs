ccba82feb44b61445933ec8684418d74c7739cc2
Sebasugami <sebastianrodriguezrodriguez06@gmail.com>
2023-06-26 12:49:19 +0200

    Camera 1 Prototype

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -11,19 +11,32 @@ public class PlayerLook : MonoBehaviour
 
     //Posición del ratón
     private Vector2 mouseDelta;
+
+    private int maxY = 90;
+    private int minY = -85;
     
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+    void LateUpdate()
+    {
+        cameraLook();
+    }
+
     private void cameraLook()
     {
+        camRot += mouseDelta.y * sensConfig.CamSensY;
+        camRot = Mathf.Clamp(camRot, minY, maxY);
+        CamHolder.localEulerAngles = new Vector3(-camRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * sensConfig.CamSensX, 0);
 
     }
 
-    void LateUpdate()
+    public void OnlookInput(InputAction.CallbackContext context)
     {
-        
+        mouseDelta = context.ReadValue<Vector2>();
     }
 }
