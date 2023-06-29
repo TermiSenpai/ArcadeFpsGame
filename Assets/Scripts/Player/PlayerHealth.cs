5bc44c57f4b1303c519b918ff8dda0b8f8c3a975
TermiSenpai <escarpaprieto@gmail.com>
2023-06-29 11:29:48 +0200

    Kill and respawn enemy player

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,26 +6,40 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     PhotonView pv;
+    PlayerManager playerManager;
+    private const float maxHealth = 100f;
+    private float currentHealth;
 
     private void Awake()
     {
         pv = GetComponent<PhotonView>();
+        playerManager = PhotonView.Find((int) pv.InstantiationData[0]).GetComponent<PlayerManager>();
     }
 
     private void Start()
     {
+        currentHealth = maxHealth;
     }
 
     public void takeDamage(float damage)
     {
-        pv.RPC("RPC_TackeDamage", RpcTarget.All, damage, PhotonNetwork.NickName);
+        pv.RPC("RPC_TackeDamage", RpcTarget.All, damage);
     }
 
     [PunRPC]
-    void RPC_TackeDamage(float damage, string nick)
+    void RPC_TackeDamage(float damage)
     {
         if (!pv.IsMine) return;
 
-        Debug.Log($"damaged by: {nick}");
+
+
+        currentHealth -= damage;
+
+        if(currentHealth <= 0) playerDie();
+    }
+
+    private void playerDie()
+    {
+        playerManager.die();
     }
 }
