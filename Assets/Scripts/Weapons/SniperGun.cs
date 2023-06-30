8ae517babaefda718767c931feb4f5c28d5c78a9
TermiSenpai <escarpaprieto@gmail.com>
2023-06-30 10:43:02 +0200

    bullet impact with rotation adapted

--- Assets/Scripts/Weapons/SniperGun.cs
+++ Assets/Scripts/Weapons/SniperGun.cs
@@ -28,7 +28,7 @@ public class SniperGun : Gun
         Ray r = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
         r.origin = cam.transform.position;
 
-        if(Physics.Raycast(r, out RaycastHit hit))
+        if (Physics.Raycast(r, out RaycastHit hit))
         {
             Debug.Log(hit.collider.gameObject.name);
             hit.collider.gameObject.GetComponent<IDamageable>()?.takeDamage(((GunInfo)itemInfo).damage);
@@ -41,8 +41,16 @@ public class SniperGun : Gun
     {
         //TODO
         // Change instantiate for gameobject enable
+        Collider[] colliders = Physics.OverlapSphere(hitPos, 0.3f);
+        if (colliders.Length != 0)
+        {
+            GameObject impact = Instantiate(bulletImpactPrefab, hitPos + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+            Destroy(impact, 5f);
+            impact.transform.SetParent(colliders[0].transform);
 
-        Instantiate(bulletImpactPrefab, hitPos + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
-        Debug.Log("Instantiated impact");
+        }
+
+
+
     }
 }
