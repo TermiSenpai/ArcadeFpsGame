1d0333f44f5e1949865e4da807bef194126bd229
TermiSenpai <escarpaprieto@gmail.com>
2023-07-18 10:44:15 +0200

    better sens and volume sliders script

--- Assets/Scripts/Settings/SensitivitySettings.cs
+++ Assets/Scripts/Settings/SensitivitySettings.cs
@@ -7,36 +7,58 @@ using UnityEngine.UI;
 public class SensitivitySettings : MonoBehaviour
 {
     [SerializeField] PlayerSensConfig sensConfig;
+    [Header("Sliders")]
     [SerializeField] Slider normalSlider;
     [SerializeField] Slider aimSlider;
+    [Header("Texts")]
     [SerializeField] TMP_Text normalTxt;
     [SerializeField] TMP_Text aimTxt;
 
+    private const string normalName = "normalSens";
+    private const string aimName = "aimSens";
+
     private void Start()
     {
-        sensConfig.sensitivity = PlayerPrefs.GetFloat("normalSens");
-        sensConfig.aimSens = PlayerPrefs.GetFloat("aimSens");
+        loadSavedValues();
+        setValueToTxt();
+        addSliderListeners();
     }
 
-    public void setNormalSens(float newValue) => sensConfig.sensitivity = newValue;
-    public void setAimSens(float newValue) => sensConfig.aimSens = newValue;
+    private void loadSavedValues()
+    {
+        float normalSens = PlayerPrefs.GetFloat(normalName, 0.5f);
+        float aimSens = PlayerPrefs.GetFloat(aimName, 0.5f);
+
+        sensConfig.sensitivity = normalSens;
+        sensConfig.aimSens = aimSens;
+
+        normalSlider.value = normalSens;
+        aimSlider.value = aimSens;
+    }
+
+    private void addSliderListeners()
+    {
+        normalSlider.onValueChanged.AddListener(setNormalSens);
+        aimSlider.onValueChanged.AddListener(setAimSens);
+    }
+
+    public void setNormalSens(float newValue)
+    {
+        sensConfig.sensitivity = newValue;
+        setValueToTxt();
+        PlayerPrefs.SetFloat(normalName, sensConfig.sensitivity);
+    }
+    public void setAimSens(float newValue)
+    {
+        sensConfig.aimSens = newValue;
+        setValueToTxt();
+        PlayerPrefs.SetFloat(aimName, sensConfig.aimSens);
+    }
 
     public void setValueToTxt()
     {
         normalTxt.text = (normalSlider.value * 10).ToString("0.00");
         aimTxt.text = (aimSlider.value * 10).ToString("0.00");
-    }
-
-    private void OnEnable()
-    {
-        normalSlider.value = sensConfig.sensitivity;
-        aimSlider.value = sensConfig.aimSens;
-    }
-
-    private void OnDisable()
-    {
-        PlayerPrefs.SetFloat("normalSens", normalSlider.value);
-        PlayerPrefs.SetFloat("aimSens", aimSlider.value);
     }
 
 
--- Assets/Scripts/UI/Audio/VolumeSettings.cs
+++ Assets/Scripts/UI/Audio/VolumeSettings.cs
@@ -6,23 +6,45 @@ using UnityEngine.Audio;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private Slider slider;
     [SerializeField] private AudioMixer mixer;
-    [SerializeField] private string mixerName;
+    [Header("Sliders")]
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider fxSlider;
+
+    // mixer exposed names
+    private const string masterName = "MasterVolume";
+    private const string musicName = "MusicVolume";
+    private const string fxName = "FxVolume";
+    
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey(mixerName))
-        {
-            slider.value = PlayerPrefs.GetFloat(mixerName, 0.5f);
-            setVolume(slider.value);
-        }
-
+        loadSaveValues();
+        addSliderListeners();
     }
 
-    public void setVolume(float sliderValue)
+    private void loadSaveValues()
     {
-        mixer.SetFloat(mixerName, Mathf.Log10(sliderValue) * 20);
-        PlayerPrefs.SetFloat(mixerName, sliderValue);
+        float masterVolume = PlayerPrefs.GetFloat(masterName, 0.5f);
+        float musicVolume = PlayerPrefs.GetFloat(musicName, 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat(fxName, 0.5f);
+
+        masterSlider.value = masterVolume;
+        musicSlider.value = musicVolume;
+        fxSlider.value = sfxVolume;
+    }
+
+    private void addSliderListeners()
+    {
+        masterSlider.onValueChanged.AddListener(delegate{ SetVolume(masterName, masterSlider.value); });
+        musicSlider.onValueChanged.AddListener(delegate { SetVolume(musicName, musicSlider.value); });
+        fxSlider.onValueChanged.AddListener(delegate { SetVolume(fxName, fxSlider.value); });
+    }
+
+    private void SetVolume(string volumeType, float volume)
+    {
+        mixer.SetFloat(volumeType, Mathf.Log10(volume) * 20f);
+        PlayerPrefs.SetFloat(volumeType, volume);
     }
 }
